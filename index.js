cf25c660f4d14fbae6435f23037b0f8f0c10423d
Juliano <exZ@users.noreply.github.com>
2016-07-14 14:17:21 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+export default class CouchbasePro {
+    constructor(deps, config = {}) {
+        this._couchbase = deps.couchbase || require('couchbase');
+        this._cluster = new this._couchbase.Cluster(config.cluster);
+        this._N1qlQuery = this._couchbase.N1qlQuery;
+        this.bucket = this._cluster.openBucket(config.bucket, config.password);
+        Object.assign(this.bucket, config.timeouts);
+    }
+
+    _invoke(m, ...a) {
+        return new Promise((o, f) => {
+            this.bucket[m](...a, (e, r) => {
+                if (e) { return f(e); } else { return o(r); }
+            });
+        });
+    }
+
+    read (...a) { return this._invoke('get', ...a); }
+    query (...a) { return this._invoke('query', ...a); }
+    touch (...a) { return this._invoke('touch', ...a); }
+    count (...a) { return this._invoke('counter', ...a); }
+    upsert (...a) { return this._invoke('upsert', ...a) }
+    insert (...a) { return this._invoke('insert', ...a); }
+    remove (...a) { return this._invoke('delete', ...a); }
+    append (...a) { return this._invoke('append', ...a); }
+    unlock (...a) { return this._invoke('unlock', ...a) }
+    replace (...a) { return this._invoke('replace', ...a); }
+    prepend (...a) { return this._invoke('prepend', ...a); }
+    readMany (...a) { return this._invoke('getMulti', ...a); }
+    lookupIn (...a) { return this._invoke('lookupIn', ...a); }
+    mutateIn (...a) { return this._invoke('mutateIn', ...a); }
+    getAndLock (...a) { return this._invoke('getAndLock', ...a); }
+    getAndTouch (...a) { return this._invoke('getAndTouch', ...a); }
+    getFromReplica (...a) { return this._invoke('getFromReplica', ...a); }
+
+}
